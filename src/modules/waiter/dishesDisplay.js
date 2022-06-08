48d164987f4d22ee4c07403886eb1682baf848cc
kennAnyUM <40307932+kennAnyUM@users.noreply.github.com>
2022-06-08 12:55:34 -0600

    OrderDetails change

--- src/modules/waiter/dishesDisplay.js
+++ src/modules/waiter/dishesDisplay.js
@@ -6,25 +6,19 @@ import {
   ListGroup,
   Button,
   FormControl,
-  Form,
+  InputGroup,
 } from "react-bootstrap";
 
 export default function DishesDisplay() {
   const API_GET = "http://localhost:4000/waiter/Dishes/GetAllDishes";
-  const API_URL = "http://localhost:4000/Administration/Admin/InsertOrder";
-  const API_URL_Modify =
-    "http://localhost:4000/Administration/Admin/ModifyOrder";
   const API_URL_INSERT_OD =
-    "http://localhost:4000/Administration/Admin/InsertOrder_Details";
+    "http://localhost:4000/waiter/Dishes/InsertOrderDetails";
   const [resDishesData, setresDishesData] = useState([]);
   const [postDataOrderDetails, setpostDataOrderDetails] = useState([]);
   const [dataExists, setdataExists] = useState(true);
-  const [isOrderCreated, setisOrderCreated] = useState(false);
   const [newNumberTable, setnewNumberTable] = useState(0);
   const [newOrderDescription, setnewOrderDescription] = useState("");
   const [newOrderDate, setnewOrderDate] = useState(new Date());
-  const [newOrderID, setnewOrderID] = useState(0);
-  const [isModify, setisModify] = useState(false);
 
   const getAllDishesRequest = async () => {
     try {
@@ -45,12 +39,17 @@ export default function DishesDisplay() {
     );
     if (alreadyExists === undefined) {
       let newOrder = {
-        fK_Order_Order_Details: newOrderID,
+        fK_Order_Order_Details: 0,
         fK_Plato_Bebida_Order_Details: resultado.platoID,
         fK_Plato_Bebida_Order_Details_Name: resultado.plato_Bebida_Nombre,
         order_Details_Amount: 1,
         order_Details_Description:
-          "Mesa: " + newNumberTable + " - Fecha: " + newOrderDate.toLocaleString('es-CR', {timeZone: 'America/Costa_Rica'}),
+          "Mesa: " +
+          newNumberTable +
+          " - Fecha: " +
+          newOrderDate.toLocaleString("es-CR", {
+            timeZone: "America/Costa_Rica",
+          }),
         order_Details_Date: newOrderDate,
       };
       setpostDataOrderDetails([...postDataOrderDetails, newOrder]);
@@ -86,89 +85,24 @@ export default function DishesDisplay() {
   };
 
   const InsertOrderDetailsEvent = async () => {
-    console.log({ postDataOrderDetails });
+    let dataObj = {
+      Numero_Mesa: newNumberTable,
+      ListOrderDetails: postDataOrderDetails
+    }
+    console.log({ dataObj });
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(postDataOrderDetails),
+      body: JSON.stringify(dataObj),
     };
     console.log(requestOptions);
     const response = await fetch(API_URL_INSERT_OD, requestOptions);
     if (response.status === 200) {
       alert("Se agregaron los detalles de la orden.");
-      setisModify(false);
-      setisOrderCreated(false);
       setpostDataOrderDetails([]);
       setnewNumberTable(0);
-      setnewOrderID(0);
     } else {
       alert("Ocurrio un error: " + response.status);
-    }
-  };
-
-  const InsertOrderEvent = async () => {
-    if (isModify) {
-      if (newOrderID === 0) {
-        alert(
-          "No se cargaron los datos de forma adecuada, falta el identificador de la orden."
-        );
-      } else {
-        if (!isNaN(newNumberTable) || newNumberTable === 0) {
-          alert("No ingrese letras en el numero de mesa o modifique el numero de la mesa.");
-        } else {
-          let obj = {
-            Order_ID: newOrderID,
-            Order_Date: newOrderDate,
-            Numero_Mesa: newNumberTable,
-            Order_Description: "Mesa: " + newNumberTable + " - Fecha: " + newOrderDate.toLocaleString('es-CR', {timeZone: 'America/Costa_Rica'}),
-            Order_Total_Amount: 0,
-          };
-          handleInsertNewOrder(obj);
-        }
-      }
-    } else {
-      if (isNaN(newNumberTable) || newNumberTable === 0) {
-        alert("No ingrese letras en el numero de mesa o modifique el numero de la mesa.");
-      } else {
-        let obj = {
-          Order_Date: newOrderDate,
-          Numero_Mesa: newNumberTable,
-          Order_Description:"Mesa: " + newNumberTable + " - Fecha: " + newOrderDate.toLocaleString('es-CR', {timeZone: 'America/Costa_Rica'}),
-          Order_Total_Amount: 0,
-        };
-        handleInsertNewOrder(obj);
-      }
-    }
-  };
-
-  const handleInsertNewOrder = async (obj) => {
-    console.log(obj);
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(obj),
-    };
-    if (isModify) {
-      const response = await fetch(API_URL_Modify, requestOptions);
-      const data = await response.json();
-      setnewOrderID(data);
-      if (response.status === 200) {
-        alert("Se Modifico correctamente");
-      } else {
-        alert("Ocurrio un error: " + response.status);
-      }
-    } else {
-      const response = await fetch(API_URL, requestOptions);
-      const data = await response.json();
-      setnewOrderID(data);
-      if (response.status === 200) {
-        alert("Se ingreso la orden correctamente");
-        setisOrderCreated(true);
-        setisModify(true);
-        setnewOrderDescription("");
-      } else {
-        alert("Ocurrio un error: " + response.status);
-      }
     }
   };
 
@@ -182,93 +116,89 @@ export default function DishesDisplay() {
         <Container>
           <Row>
             <Col xs={4} xl={4}>
-              <label>Numero de orden: {newOrderID}</label>
-              <Form.Group className="mb-3" controlId="formBasicNumberTable">
-                <Form.Label>Numero de mesa</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese el numero de la mesa."
-                  value={newNumberTable}
-                  onChange={(event) => setnewNumberTable(event.target.value)}
-                />
-              </Form.Group>
-              <Button onClick={InsertOrderEvent}>
-                {isOrderCreated ? "Modificar orden" : "Crear orden"}
-              </Button>
+              <div>
+                <InputGroup>
+                  <InputGroup.Text>Numero de mesa</InputGroup.Text>
+                  <FormControl
+                    placeholder="Numero de mesa"
+                    aria-label="TableNumber"
+                    values={newNumberTable}
+                    onChange={(event) =>
+                      setnewNumberTable(event.target.value)}
+                  />
+                </InputGroup>
+              </div>
             </Col>
           </Row>
-          <br />
-          {!isOrderCreated ? (
-            <Form.Label>
-              Ingresa una nueva orden con el numero de mesa.
-            </Form.Label>
-          ) : (
-            <Row>
-              <Col xs={6} xl={6}>
-                <ListGroup>
-                  {resDishesData.map((AllDishes) => (
-                    <Row key={AllDishes.platoID}>
-                      <Col xs={6} xl={6}>
-                        <ListGroup.Item>
-                          {AllDishes.plato_Bebida_Nombre}
-                        </ListGroup.Item>
-                      </Col>
-                      <Col xs={2} xl={2}></Col>
-                      <Col xs={4} xl={4}>
-                        <Button
-                          onClick={(event) => handleAddDish(event.target.value)}
-                          value={AllDishes.platoID}
-                        >
-                          Agregar
-                        </Button>
-                      </Col>
-                    </Row>
-                  ))}
-                </ListGroup>
-              </Col>
-              <Col xs={6} xl={6}>
-                <ListGroup>
-                  {postDataOrderDetails.map((AllDishesToPost) => (
-                    <Row key={AllDishesToPost.fK_Plato_Bebida_Order_Details}>
-                      <Col xs={6} xl={6}>
-                        <ListGroup.Item>
-                          {AllDishesToPost.fK_Plato_Bebida_Order_Details_Name}
-                        </ListGroup.Item>
-                      </Col>
-                      <Col xs={2} xl={2}>
-                        <FormControl
-                          size="lg"
-                          type="text"
-                          placeholder="Cantidad"
-                          value={AllDishesToPost.order_Details_Amount}
-                          onChange={(event) =>
-                            handleUpdateDish(
-                              AllDishesToPost.fK_Plato_Bebida_Order_Details,
-                              event.target.value
-                            )
-                          }
-                        />
-                      </Col>
-                      <Col xs={4} xl={4}>
-                        <Button
-                          onClick={(event) =>
-                            handleDeleteDish(
-                              AllDishesToPost.fK_Plato_Bebida_Order_Details
-                            )
-                          }
-                        >
-                          Eliminar
-                        </Button>
-                      </Col>
-                    </Row>
-                  ))}
-                </ListGroup>
-              </Col>
+          <Row>
+            <Col xs={6} xl={6}>
+              <ListGroup>
+                {resDishesData.map((AllDishes) => (
+                  <Row key={AllDishes.platoID}>
+                    <Col xs={6} xl={6}>
+                      <ListGroup.Item>
+                        {AllDishes.plato_Bebida_Nombre}
+                      </ListGroup.Item>
+                    </Col>
+                    <Col xs={2} xl={2}></Col>
+                    <Col xs={4} xl={4}>
+                      <Button
+                        onClick={(event) => handleAddDish(event.target.value)}
+                        value={AllDishes.platoID}
+                      >
+                        Agregar
+                      </Button>
+                    </Col>
+                  </Row>
+                ))}
+              </ListGroup>
+            </Col>
+            <Col xs={6} xl={6}>
+              <ListGroup>
+                {postDataOrderDetails.map((AllDishesToPost) => (
+                  <Row key={AllDishesToPost.fK_Plato_Bebida_Order_Details}>
+                    <Col xs={6} xl={6}>
+                      <ListGroup.Item>
+                        {AllDishesToPost.fK_Plato_Bebida_Order_Details_Name}
+                      </ListGroup.Item>
+                    </Col>
+                    <Col xs={2} xl={2}>
+                      <FormControl
+                        size="lg"
+                        type="text"
+                        placeholder="Cantidad"
+                        value={AllDishesToPost.order_Details_Amount}
+                        onChange={(event) =>
+                          handleUpdateDish(
+                            AllDishesToPost.fK_Plato_Bebida_Order_Details,
+                            event.target.value
+                          )
+                        }
+                      />
+                    </Col>
+                    <Col xs={4} xl={4}>
+                      <Button
+                        onClick={(event) =>
+                          handleDeleteDish(
+                            AllDishesToPost.fK_Plato_Bebida_Order_Details
+                          )
+                        }
+                      >
+                        Eliminar
+                      </Button>
+                    </Col>
+                  </Row>
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} xl={12}>
               <Button onClick={InsertOrderDetailsEvent}>
                 Agregar detalles de la orden.
               </Button>
-            </Row>
-          )}
+            </Col>
+          </Row>
         </Container>
       </React.Fragment>
     );
