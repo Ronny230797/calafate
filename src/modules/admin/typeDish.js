7cff61572fba972601a62731885bb72fe464730e
Ronny230797 <ronny230797@gmail.com>
2022-07-28 12:06:39 -0600

    modificar eliminar buttons

--- src/modules/admin/typeDish.js
+++ src/modules/admin/typeDish.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Table, Input } from "antd";
+import { Table, Input, Space, Tag } from "antd";
 import axios from "axios";
 import { userColumns } from "../../components/columns";
 import { useTableSearch } from "../../components/useTableSearch";
 
 const { Search } = Input;
-
+const { Column } = Table;
 const fetchUsers = async () => {
   const { data } = await axios.get(
     "http://localhost:4000/Administration/Admin/GetAllTypeDishDrink"
@@ -31,12 +31,27 @@ export default function App() {
       />
       <br /> <br />
       <Table
-        rowKey="tipo_Plato_Bebida_ID"
         dataSource={filteredData}
-        columns={userColumns}
-        loading={loading}
         pagination={false}
-      />
+      >
+        <Column title="Id" dataIndex="tipo_Plato_Bebida_ID" key="tipo_Plato_Bebida_ID" />
+        <Column title="Tipo" dataIndex="tipo_Plato_Bebida_Name" key="tipo_Plato_Bebida_Name" />
+        <Column title="Descripcion" dataIndex="tipo_Plato_Bebida_Description" key="tipo_Plato_Bebida_Description" />
+        <Column title="Modificar" key="modificar"
+          render={(_, record) => (
+            <Space size="middle">
+              <button>Modificar {record.tipo_Plato_Bebida_Name}</button>
+            </Space>
+          )}
+        />
+        <Column title="Eliminar" key="eliminar"
+          render={(_, record) => (
+            <Space size="middle">
+              <button>Eliminar {record.tipo_Plato_Bebida_Name}</button>
+            </Space>
+          )}
+        />
+      </Table>
     </>
   );
 }
