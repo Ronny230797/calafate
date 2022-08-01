bde3b3a1693f276375f2579a6e59d664d64d1205
Ronny230797 <ronny230797@gmail.com>
2022-08-01 11:02:50 -0600

    Sales center mesas

--- src/modules/generic/SalesCenter.js
+++ src/modules/generic/SalesCenter.js
@@ -2,31 +2,58 @@ import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../../styles/admin/admin.scss";
+import "../../styles/admin/table-order.scss"
 
 export default function SalesCenter() {
-    return (
-        <React.Fragment>
-            <Container className="admin">
-            <Row>
-          <Col xs={12} md={4}>
-            <div className="card-cotainer">
-              <Link to="/">Administrador de Usuarios</Link>
-            </div>
-          </Col>
-          <Col xs={12} md={4}>
-            <div className="card-cotainer">
-              <p>
-                <Link to="/">Administrador de Platos</Link>
-              </p>
-            </div>
-          </Col>
-          <Col xs={12} md={4}>
-            <div className="card-cotainer">
-              <Link to="/">Administrador de Venta</Link>
-            </div>
-          </Col>
+  const n1 = 1;
+  const n2 = 4;
+  const n3 = 7;
+
+
+  const ClickTable = (i) => {
+    alert(`Mesa ${i}`)
+  }
+
+  return (
+    <React.Fragment>
+      <Container className="admin">
+        <Row>
+          {
+            Array.apply(null, { length: 3 }).map((e, i) => (
+              <Col xs={12} md={4}>
+                <div className="table-order" key={(n1 + i)} onClick={(e) => ClickTable((n1 + i))}>
+                  <h1>Mesa</h1>
+                  <h3>{(n1 + i)}</h3>
+                </div>
+              </Col>
+            ))
+          }
         </Row>
-            </Container>
-        </React.Fragment>
-    );
+        <Row>
+          {
+            Array.apply(null, { length: 3 }).map((e, i) => (
+              <Col xs={12} md={4}>
+                <div className="table-order" key={(n2 + i)} onClick={(e) => ClickTable((n2 + i))}>
+                  <h1>Mesa</h1>
+                  <h3>{(n2 + i)}</h3>
+                </div>
+              </Col>
+            ))
+          }
+        </Row>
+        <Row>
+          {
+            Array.apply(null, { length: 3 }).map((e, i) => (
+              <Col xs={12} md={4}>
+                <div className="table-order" key={(n3 + i)} onClick={(e) => ClickTable((n3 + i))}>
+                  <h1>Mesa</h1>
+                  <h3>{(n3 + i)}</h3>
+                </div>
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
 }
